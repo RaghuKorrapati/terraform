76c2c26dbc5d513bf88b8439ab2b295e4b72d11c
Raghu <raghukorrapati16@gmail.com>
2022-03-04 14:36:50 +0530

    commit

--- examples/s3/main.tf
+++ examples/s3/main.tf
@@ -1,11 +1,13 @@
 resource "aws_s3_bucket" "b" {
   bucket = "var.bucket"
-  
+  acl = "private"
+   versioning {
+      enabled = true
+   }
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
 }
-
 
 
