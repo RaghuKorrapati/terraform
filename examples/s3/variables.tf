a1f3286ddea32490e9a3ec242345af6e552b66e7
Raghu <raghukorrapati16@gmail.com>
2022-03-04 14:21:36 +0530

    commit

---
+++ examples/s3/variables.tf
@@ -0,0 +1,14 @@
+variable "acl" {
+  type        = string
+  default     = "public-read-write"
+  
+}
+variable "bucket" {
+  type = string
+  default = "mys3korrapatiraghukesavanaidu1451"
+}
+
+variable "location" {
+  type = string
+  default = "ap-east-1"
+}
